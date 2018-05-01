c50b61ef301ce583db2aef344c02b96a6098f5ee
libufan <bufan.li@bkjk.com>
2018-05-01 11:38:50 +0800

    add solution

---
+++ RegularExpressionMatching.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  if (p.length === 0) return Boolean(!s.length);
+  if (p.length > 1 && p[1] == '*') {
+    return isMatch(s, p.substr(2)) || (!(s.length === 0) && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p));
+  } else {
+    return !(s.length === 0) && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+  }
+};
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  var r = new RegExp("^" + p + "$")
+  return r.test(s);
+};
