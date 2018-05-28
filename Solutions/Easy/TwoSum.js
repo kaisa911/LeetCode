b887092f1baebe0a1b1e6fed4a6938b4eb0e736a
libufan <bufan.li@bkjk.com>
2018-05-28 16:08:26 +0800

    change

--- Solutions/Easy/TwoSum.js
+++ Solutions/Easy/TwoSum.js
@@ -3,23 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-	let i = 0,
-		len = nums.length,
-		hash = {},
-		res = [],
-		t1, t2;
-	while (i < len) {
-		t2 = len - i - 1;
-		hash[nums[t2]] = t2;
-		t1 = hash[target - nums[i]];
-		if (t1 !== undefined && t1 !== i) {
-			res.push(i);
-			(i > t1) ? res.unshift(t1) : res.push(t1);
-			break;
+const twoSum = (nums, target) => {
+	const len = nums.length
+	const map = {}
+	for (let i = 0; i < len; i++) {
+		const curr = nums[i]
+		const j = map[target - curr]
+		if (j != null) {
+			return [j, i]
 		}
-		hash[nums[i]] = i;
-		i++
+		map[curr] = i
 	}
-	return res;
-};
+}
