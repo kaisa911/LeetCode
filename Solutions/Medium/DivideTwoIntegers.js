48046b8a2a69755b5345ee0e6164d5fe4f452616
libufan <bufan.li@bkjk.com>
2018-06-12 16:56:04 +0800

    add solution

--- Solutions/Medium/DivideTwoIntegers.js
+++ Solutions/Medium/DivideTwoIntegers.js
@@ -6,19 +6,16 @@
 var divide = function(dividend, divisor) {
   let MAX_VALUE = Math.pow(2, 31) - 1,
     MIN_VALUE = -Math.pow(2, 31),
-    flag = 0,
+    flag = 1,
     res;
 
   res = Math.floor(Math.abs(dividend) / Math.abs(divisor));
-  if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
-    flag = 1;
-  } else {
+  if ((dividend > 0 && divisor < 0) || !(dividend < 0 && divisor > 0)) {
     flag = -1;
     res = res * flag;
   }
 
   if (res < MIN_VALUE) res = MIN_VALUE;
-
   if (res > MAX_VALUE) res = MAX_VALUE;
 
   return res;
