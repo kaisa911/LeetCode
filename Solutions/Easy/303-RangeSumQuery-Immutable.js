65bc84bfdb35104a8f7ea2c40596e54b44ee50a8
bufan.li <lbf911@126.com>
2019-09-06 11:38:20 +0800

    add solution 303

---
+++ Solutions/Easy/303-RangeSumQuery-Immutable.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ */
+
+var NumArray = function(nums) {
+  this.sum = [];
+  this.sum[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    this.sum[i] = this.sum[i - 1] + nums[i];
+  }
+};
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+
+NumArray.prototype.sumRange = function(i, j) {
+  return i == 0 ? this.sum[j] : this.sum[j] - this.sum[i - 1];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
