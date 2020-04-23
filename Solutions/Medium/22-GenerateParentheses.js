7a13b715e49957268ace476a36cdcd8959a58c63
bufan.li <lbf911@126.com>
2020-04-23 13:59:08 +0800

    change 22

--- Solutions/Medium/22-GenerateParentheses.js
+++ Solutions/Medium/22-GenerateParentheses.js
@@ -2,18 +2,19 @@
  * @param {number} n
  * @return {string[]}
  */
-const generateParenthesis = (n) => {
-  let res = [];
-  Helper(n, n, '', res);
+var generateParenthesis = function (n) {
+  const res = [];
+  if (n === 0) return res;
+
+  const backTrace = (left, right, current) => {
+    if (left > right) return;
+    if ((left === 0) & (right === 0)) {
+      res.push(current);
+      return;
+    }
+    if (left > 0) backTrace(left - 1, right, current + '(');
+    if (right > 0) backTrace(left, right - 1, current + ')');
+  };
+  backTrace(n, n, '');
   return res;
 };
-
-const Helper = (left, right, str, res)=> {
-  if (left > right) return;
-  if ((left == 0) & (right == 0)) {
-    res.push(str);
-  } else {
-    if (left > 0) Helper(left - 1, right, str + '(', res);
-    if (right > 0) Helper(left, right - 1, str + ')', res);
-  }
-}
---
+++ Thinkings/Medium/22-GenerateParentheses.md
@@ -0,0 +1,44 @@
+# 括号生成
+
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+**示例：**
+
+```
+输入：n = 3
+输出：[
+"((()))",
+"(()())",
+"(())()",
+"()(())",
+"()()()"
+]
+```
+
+**思路：**
+运用回溯思想：
+声明一个回溯方法，退出条件是左边的值大于右边的值，或者左右两边的值都为 0；
+然后判断，如果哪边大于 0 就走相应-1 继续回溯。
+
+```js
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const res = [];
+  if (n === 0) return res;
+
+  const backTrace = (left, right, current) => {
+    if (left > right) return;
+    if ((left === 0) & (right === 0)) {
+      res.push(current);
+      return;
+    }
+    if (left > 0) backTrace(left - 1, right, current + '(');
+    if (right > 0) backTrace(left, right - 1, current + ')');
+  };
+  backTrace(n, n, '');
+  return res;
+};
+```
