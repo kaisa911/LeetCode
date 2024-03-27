e467ac2a5a4fe73747b4ee75e6f5c3f3535dc133
啪叽 <lbf911@126.com>
2024-03-27 21:51:09 +0800

    feat: update solution

--- Solutions/Hard/149-MaxPointsonaLine.js
+++ Solutions/Hard/149-MaxPointsonaLine.js
@@ -39,3 +39,19 @@ var maxPoints = function (points) {
 const gcd = (a, b) => {
   return b != 0 ? gcd(b, a % b) : a;
 };
+
+var maxPoints = function (points) {
+  let max = 1;
+  for (let i = 0; i < points.length; i++) {
+    let map = new Map();
+    for (let j = 0; j < points.length; j++) {
+      if (i === j) continue;
+      let k = (points[i][0] - points[j][0]) / (points[i][1] - points[j][1]);
+      map.set(k, map.has(k) ? map.get(k) + 1 : 2);
+    }
+    for (let val of map.values()) {
+      max = Math.max(max, val);
+    }
+  }
+  return max;
+};
